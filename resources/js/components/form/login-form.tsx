d81640f35872370f0fd832b12e88ebb3a1f67529
iccangji <iksanpml131@gmail.com>
2025-07-13 00:21:36 +0800

    update login page

--- resources/js/components/form/login-form.tsx
+++ resources/js/components/form/login-form.tsx
@@ -93,6 +93,7 @@ export function LoginForm({
           <div className="relative hidden bg-muted md:block">
             <img
               src="/images/truck.png"
+              loading="lazy"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
